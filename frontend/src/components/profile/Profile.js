393cd9ec3ecdefb18ec76c39cf6281d64429f144
Taryn Chiang <tarynchiang@email.com>
2023-01-17 17:31:05 +0800

    update style

--- frontend/src/components/profile/Profile.js
+++ frontend/src/components/profile/Profile.js
@@ -1,89 +1,87 @@
-import React from 'react';
-import '../../App.css';
-import axios from 'axios';
-import {Link} from 'react-router-dom';
-import EditCocktail from '../editcocktail/EditCocktail.js';
-class Profile extends React.Component{
+import React from "react";
+import "../../App.scss";
+import axios from "axios";
+import { Link } from "react-router-dom";
+import EditCocktail from "../editcocktail/EditCocktail.js";
+class Profile extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      editing: false,
+    };
+  }
 
-    constructor(props){
-        super(props)
-        this.state = {
-            editing: false,
-        }
+  changeEditing = (whichNumber) => {
+    this.setState({ editing: whichNumber });
+  };
+
+  resetEdit = () => {
+    this.setState({ editing: false });
+  };
+
+  deleteCocktail = (idOfCocktail) => {
+    axios
+      .post(`${process.env.REACT_APP_API_URL}/delete-cocktail/` + idOfCocktail)
+      .then(() => {
+        this.props.getMyCocktail();
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
+  showMyCocktail = () => {
+    const myCocktails = this.props.AlltheCocktails.filter((eachP) => {
+      return eachP.owner._id === this.props.theUser._id;
+    });
+
+    return myCocktails.map((eachCocktail, index) => {
+      if (this.state.editing !== index) {
+        return (
+          <div className="eachRecipe">
+            <Link exact to={`/recipedetails/${eachCocktail._id}`}>
+              <img src={eachCocktail.img} alt="cocktail-img" />
+              <h1>{eachCocktail.name}</h1>
+            </Link>
+
+            <div className="delete-edit">
+              <Link exact to={`/editCocktail/${eachCocktail._id}`}>
+                edit
+              </Link>
+              <button onClick={() => this.deleteCocktail(eachCocktail._id)}>
+                delete
+              </button>
+            </div>
+          </div>
+        );
+      } else {
+        return (
+          <EditCocktail
+            resetEditingSituation={this.resetEdit}
+            theCocktail={eachCocktail}
+            getAllTheCocktailInAppJS={this.props.getMyCocktail}
+          />
+        );
+      }
+    });
+  };
+
+  render() {
+    console.log("-=-=-=--=", this.props.AlltheCocktails);
+
+    if (this.props.myCocktailReady) {
+      return (
+        <div className="recipe-page">
+          {this.showMyCocktail()}
+          <Link exact to="/createCocktail" className="add-pic">
+            <img src="/images/add.png" alt="add-sign" />
+          </Link>
+        </div>
+      );
+    } else {
+      return <h1>Loading...</h1>;
     }
-
-    changeEditing = (whichNumber) => {
-        this.setState({editing: whichNumber})
-    } 
-
-    resetEdit = () =>{
-        this.setState({editing: false})
-    }
-
-    deleteCocktail = (idOfCocktail) =>{
-    
-        axios.post(`${process.env.REACT_APP_API_URL}/delete-cocktail/`+idOfCocktail)
-        .then(()=>{
-            this.props.getMyCocktail();
-        })
-        .catch((err)=>{
-            console.log(err)
-        })
-    }
-
-    showMyCocktail = () =>{
-        const myCocktails = this.props.AlltheCocktails.filter((eachP)=>{
-            return eachP.owner._id === this.props.theUser._id;
-        })
-
-        return myCocktails.map((eachCocktail,index)=>{
-            if(this.state.editing !== index){
-                return(
-                    <div className="eachRecipe">
-                        <Link exact to={`/recipedetails/${eachCocktail._id}`}>
-                            <img src={eachCocktail.img} alt="cocktail-img"/>
-                            <h1>{eachCocktail.name}</h1>
-                        </Link>
-
-                        <div className="delete-edit">
-                            <Link exact to={`/editCocktail/${eachCocktail._id}`}>
-                                edit
-                            </Link>
-                            <button onClick={()=>this.deleteCocktail(eachCocktail._id)}>delete</button>
-                        </div>
-                    </div>
-                )
-            }else{
-                return(
-                    <EditCocktail 
-                    resetEditingSituation = {this.resetEdit} 
-                    theCocktail = {eachCocktail}
-                    getAllTheCocktailInAppJS = {this.props.getMyCocktail}
-                 />
-                )
-            }
-        })
-    }
-
-    render(){
-        console.log("-=-=-=--=",this.props.AlltheCocktails)
-
-        if(this.props.myCocktailReady){
-            return(
-                <div className="recipe-page">
-                    {this.showMyCocktail()}
-                    <Link exact to = "/createCocktail" className="add-pic">
-                        < img src="/images/add.png" alt="add-sign" />
-                    </Link>
-                </div>
-            )
-        }
-        else{
-            return(
-                <h1>Loading...</h1>
-            )
-        }
-    }
+  }
 }
 
 export default Profile;
--- frontend/src/components/recipedetail/RecipeDetail.js
+++ frontend/src/components/recipedetail/RecipeDetail.js
@@ -1,91 +1,80 @@
-import React from 'react';
-import '../../App.css';
+import React from "react";
+import "../../App.scss";
 
-class RecipeDetail extends React.Component{
-  
-
-  constructor(props){
+class RecipeDetail extends React.Component {
+  constructor(props) {
     super(props);
     this.state = {
-      name:"",
-      ingredients:[],
-      instruction:"",
-      image:null,
-      ready:false
-    }
+      name: "",
+      ingredients: [],
+      instruction: "",
+      image: null,
+      ready: false,
+    };
   }
-  
-  getTheCocktail = ()=>{
+
+  getTheCocktail = () => {
     let theID = this.props.match.params.id;
-    let theCocktail = this.props.thecocktails.find((eachC)=>{
-        console.log("eachCId",eachC._id)
-        return eachC._id === theID;
-    })
+    let theCocktail = this.props.thecocktails.find((eachC) => {
+      console.log("eachCId", eachC._id);
+      return eachC._id === theID;
+    });
 
-
-    console.log("theCocktail",theCocktail)
+    console.log("theCocktail", theCocktail);
     this.setState({
-      name:theCocktail.name,
-      ingredients:theCocktail.ingredients,
-      instruction:theCocktail.instruction,
-      image:theCocktail.img,
-      ready:true,
-    })
-  }
+      name: theCocktail.name,
+      ingredients: theCocktail.ingredients,
+      instruction: theCocktail.instruction,
+      image: theCocktail.img,
+      ready: true,
+    });
+  };
 
-  showIngredients = ()=>{
-    return this.state.ingredients.map((eachIngredient)=>{
-      return(
-          <li>{eachIngredient}</li>
-      )
-  })
-  }
+  showIngredients = () => {
+    return this.state.ingredients.map((eachIngredient) => {
+      return <li>{eachIngredient}</li>;
+    });
+  };
 
-  goBack = ()=>{
-
-  }
-  showCocktailDetail = ()=>{
-    return(
+  goBack = () => {};
+  showCocktailDetail = () => {
+    return (
       <div className="Details-page">
-                <div className="Cocktail-details">
-                    <img src={this.state.image} alt="cocktail-img"/>
-                    <div className="Cocktail-info">
-                        <h1>{this.state.name}</h1>
-                        <h3>Ingredients</h3>
-                        <div className="Amount">
-                            <ul>{this.showIngredients()}</ul>
-                        </div>
-                        <h3>Instruction</h3>
-                        <p>{this.state.instruction}</p>
-                    </div>
-                </div>
-                    <button onClick={()=>{this.props.history.goBack()}} className="GoBack-btn">
-                        <img src="/images/backarrow.png" alt="goback-btn"/>
-                    </button>
+        <div className="Cocktail-details">
+          <img src={this.state.image} alt="cocktail-img" />
+          <div className="Cocktail-info">
+            <h1>{this.state.name}</h1>
+            <h3>Ingredients</h3>
+            <div className="Amount">
+              <ul>{this.showIngredients()}</ul>
             </div>
-    )
-  }
+            <h3>Instruction</h3>
+            <p>{this.state.instruction}</p>
+          </div>
+        </div>
+        <button
+          onClick={() => {
+            this.props.history.goBack();
+          }}
+          className="GoBack-btn"
+        >
+          <img src="/images/backarrow.png" alt="goback-btn" />
+        </button>
+      </div>
+    );
+  };
 
-
-  componentDidMount(){
+  componentDidMount() {
     this.getTheCocktail();
   }
 
-
-  render(){
-    if(this.state.ready){
-      return(
-        <div>
-          {this.showCocktailDetail()}
-        </div>
-      )
-    }
-    else{
-      return(
-        <h1>Loading...</h1>
-      )
+  render() {
+    if (this.state.ready) {
+      return <div>{this.showCocktailDetail()}</div>;
+    } else {
+      return <h1>Loading...</h1>;
     }
   }
 }
 
-export default RecipeDetail; 
+export default RecipeDetail;
