3ab350881f28a97549c43930d8fd0290a8fd6c59
Taryn Chiang <tarynchiang@email.com>
2023-02-01 16:27:24 +0800

    fix styles

--- frontend/src/components/cocktaildetails/CocktailDetails.js
+++ frontend/src/components/cocktaildetails/CocktailDetails.js
@@ -1,126 +1,108 @@
-import React from 'react';
-import '../../App.css';
-import axios from 'axios';
+import React from "react";
+import "../../App.scss";
+import axios from "axios";
 
-class Details extends React.Component{
+class Details extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      Cocktail: {},
+      ready: false,
+    };
+  }
 
-    constructor(props){
-        super(props);
-        this.state = {
-            Cocktail:{},
-            ready:false,
+  getCocktailDetails = (theID) => {
+    let info = {};
+    axios
+      .get("https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=" + theID)
+      .then((CocktailOB) => {
+        console.log("0-0-0-0-0-", CocktailOB.data.drinks[0]);
+        info.name = CocktailOB.data.drinks[0].strDrink;
+        info.ingredients = [];
+        info.measures = [];
+        let i = 1;
+        let Ingredient, measure;
+        let flag = true;
+        while (flag) {
+          Ingredient = CocktailOB.data.drinks[0]["strIngredient" + i];
+          measure = CocktailOB.data.drinks[0]["strMeasure" + i];
+          if (Ingredient.length === 0) {
+            flag = false;
+          } else {
+            info.ingredients.push(Ingredient);
+            info.measures.push(measure);
+          }
+          i++;
         }
+        info.instruction = CocktailOB.data.drinks[0].strInstructions;
+        info.img = CocktailOB.data.drinks[0].strDrinkThumb;
+        info.glass = CocktailOB.data.drinks[0].strGlass;
+        this.setState({ Cocktail: info, ready: true });
+      });
+  };
+
+  goBack = () => {
+    if (this.props.location.state) {
+      if (this.props.location.state.fromRefigerator) {
+        this.props.location.state.fromRefigerator = false;
+        this.props.history.push({
+          pathname: "/refrigerator",
+          state: { search: true },
+        });
+      } else {
+        this.props.history.goBack();
+      }
+    } else {
+      this.props.history.goBack();
     }
-    
-    getCocktailDetails = (theID)=>{
-        let info = {};
-        axios.get('https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i='+theID)
-        .then((CocktailOB)=>{
-            console.log('0-0-0-0-0-', CocktailOB.data.drinks[0]);
-            info.name = CocktailOB.data.drinks[0].strDrink;
-            info.ingredients = [];
-            info.measures = [];
-            let i=1;
-            let Ingredient,measure;
-            let flag=true;
-            while(flag){
-                Ingredient = CocktailOB.data.drinks[0]['strIngredient'+i];
-                measure = CocktailOB.data.drinks[0]['strMeasure'+i];
-                if(Ingredient.length === 0){
-                    flag=false;
-                }else{
-                    info.ingredients.push(Ingredient);
-                    info.measures.push(measure);
-                }
-                i++;
-            }
-            info.instruction = CocktailOB.data.drinks[0].strInstructions;
-            info.img = CocktailOB.data.drinks[0].strDrinkThumb;
-            info.glass = CocktailOB.data.drinks[0].strGlass;
-            this.setState({Cocktail: info, ready:true})
-        })
-    }
+  };
 
+  showIngredients = () => {
+    return this.state.Cocktail.ingredients.map((eachIngredient) => {
+      return <li>{eachIngredient}</li>;
+    });
+  };
 
-    goBack = ()=>{
-        if(this.props.location.state){
-            if(this.props.location.state.fromRefigerator){
-                this.props.location.state.fromRefigerator = false;
-                this.props.history.push({
-                    pathname:"/refrigerator", 
-                    state:{search:true} 
-                });
+  showAmount = () => {
+    return this.state.Cocktail.measures.map((eachMeasure) => {
+      return <li>{eachMeasure}</li>;
+    });
+  };
 
-            } else{
-                this.props.history.goBack()
-            }
-        } else{
-            this.props.history.goBack()
-        }
-
-    }
-
-
-    showIngredients = ()=>{
-        return this.state.Cocktail.ingredients.map((eachIngredient)=>{
-            return(
-                <li>{eachIngredient}</li>
-            )
-        })
-    }
-
-    showAmount = ()=>{
-        return this.state.Cocktail.measures.map((eachMeasure)=>{
-            return(
-                <li>{eachMeasure}</li>
-            )
-        })
-    }
-
-
-    showCocktailDetails = ()=>{
-        
-        return(
-            <div className="Details-page">
-                <div className="Cocktail-details">
-                    <img src={this.state.Cocktail.img}/>
-                    <div className="Cocktail-info">
-                        <h1>{this.state.Cocktail.name}</h1>
-                        <h3>Ingredients</h3>
-                        <div className="Amount">
-                            <ul>{this.showIngredients()}</ul>
-                            <ul>{this.showAmount()}</ul>
-                        </div>
-                        <h3>Instruction</h3>
-                        <p>{this.state.Cocktail.instruction}</p>
-                    </div>
-                </div>
-                    <button onClick={this.goBack} className="GoBack-btn">
-                        <img src="/images/backarrow.png" alt="goback-btn"/>
-                    </button>
+  showCocktailDetails = () => {
+    return (
+      <div className="Details-page">
+        <div className="Cocktail-details">
+          <img src={this.state.Cocktail.img} />
+          <div className="Cocktail-info">
+            <h1>{this.state.Cocktail.name}</h1>
+            <h3>Ingredients</h3>
+            <div className="Amount">
+              <ul>{this.showIngredients()}</ul>
+              <ul>{this.showAmount()}</ul>
             </div>
-        )
-    }
+            <h3>Instruction</h3>
+            <p>{this.state.Cocktail.instruction}</p>
+          </div>
+        </div>
+        <button onClick={this.goBack} className="GoBack-btn">
+          <img src="/images/backarrow.png" alt="goback-btn" />
+        </button>
+      </div>
+    );
+  };
 
-    componentDidMount(){
-        this.getCocktailDetails(this.props.match.params.id);
+  componentDidMount() {
+    this.getCocktailDetails(this.props.match.params.id);
+  }
+
+  render() {
+    if (this.state.ready) {
+      return <div>{this.showCocktailDetails()}</div>;
+    } else {
+      return <h1>Loading...</h1>;
     }
-    
-    
-    render(){
-        
-        if(this.state.ready){
-            return(
-                <div>
-                    {this.showCocktailDetails()}
-                </div>
-            )
-        }else{
-            return(
-                <h1>Loading...</h1>
-            )
-        }
-    }
+  }
 }
 
-export default Details; 
+export default Details;
